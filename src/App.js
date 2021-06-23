c97887614e254dbadb1b5c58ee555d21ba83527e
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-06-23 10:43:46 +0200

    it 5

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import IdCard from './IdCard/IdCard';
 import Greetings from './Greetings/Greeting';
 import Random from './Random/Random';
 import BoxColor from './BoxColor/BoxColor';
+import CreditCard from './CreditCard/CreditCard';
 
 
 
@@ -44,6 +45,34 @@ function App() {
 
 <BoxColor r={255} g={0} b={0} />
 <BoxColor r={128} g={255} b={0} />
+
+<CreditCard
+  type="Visa"
+  number="0123456789018845"
+  expirationMonth={3}
+  expirationYear={2021}
+  bank="BNP"
+  owner="Maxence Bouret"
+  bgColor="#11aa99"
+  color="white" />
+<CreditCard
+  type="Master Card"
+  number="0123456789010995"
+  expirationMonth={3}
+  expirationYear={2021}
+  bank="N26"
+  owner="Maxence Bouret"
+  bgColor="#eeeeee"
+  color="#222222" />
+<CreditCard
+  type="Visa"
+  number="0123456789016984"
+  expirationMonth={12}
+  expirationYear={2019}
+  bank="Name of the Bank"
+  owner="Firstname Lastname"
+  bgColor="#ddbb55"
+  color="white" />
     </div>
   );
 }
--- src/CreditCard/CreditCard.js
+++ src/CreditCard/CreditCard.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import './CeditCard.css';
+import './CreditCard.css';
 
 function CreditCard(props){
-    
-    let CardLogo = {
-        Visa : '',
-        MasterCard: ''
+
+    //logo credit card
+    let logo;
+    if(props.type === 'Visa'){
+        logo = '/img/visa.png'
+    } 
+    if(props.type === 'Master Card'){
+        logo = '/img/mastercard.png'
     }
-    
 
-    return <div>
+    //4 dernier chiffres
+    let number = props.number.substr(-4)
 
-    {props.type}
-    {props.number}
+    //date d'expiration
+    let month = props.expirationMonth
+    let year = props.expirationYear.toString().substr(-2);
+
+    if(month < 10){
+        month = '0'+ month
+    }
+
+
+
+    return <div className='card' style={{ backgroundColor : `${props.bgColor}`, color : `${props.color}`}}>
+
+    <img className ='logoCard' src={logo} alt='logo card'/>
+    <p className='cardNum'><span className='secret'>.... .... .... </span>{number}</p>
+    <p className='info'>
+        <span className='expireNum'>Expires {month}/{year} </span>
+        <span className='bank'>{props.bank}</span>
+    </p>
+    <p className='owner'>{props.owner}</p>
     
     </div>
     
